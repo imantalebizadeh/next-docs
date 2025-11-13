1887e7773cb9fcb7161c4c694c7924ebe1cd85e1
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-13 11:21:58 +0330

    feat: create extensions configuration for Tiptap editor

---
+++ src/constants/extensions.ts
@@ -0,0 +1,30 @@
+import { Highlight } from "@tiptap/extension-highlight";
+import ImageKit from "@tiptap/extension-image";
+import { TaskItem, TaskList } from "@tiptap/extension-list";
+import { TableKit } from "@tiptap/extension-table";
+import { TextAlign } from "@tiptap/extension-text-align";
+import { TextStyleKit } from "@tiptap/extension-text-style";
+import type { Extensions } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+export const extensions: Extensions = [
+  StarterKit,
+  TextStyleKit,
+  TaskList,
+  Highlight,
+  TaskItem.configure({
+    nested: true,
+  }),
+  TableKit.configure({
+    table: { resizable: true },
+  }),
+  ImageKit.configure({
+    resize: {
+      enabled: true,
+      alwaysPreserveAspectRatio: true,
+    },
+  }),
+  TextAlign.configure({
+    types: ["heading", "paragraph"],
+  }),
+];

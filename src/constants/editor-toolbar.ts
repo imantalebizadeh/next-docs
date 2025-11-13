9a179a495bb2af991a31119c3d4c39b664e44659
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-13 11:21:32 +0330

    feat: add editor toolbar constants for headings, font families, and font sizes

---
+++ src/constants/editor-toolbar.ts
@@ -0,0 +1,42 @@
+export const headings = [
+  { label: "Paragraph", value: 0 },
+  { label: "Heading 1", value: 1 },
+  { label: "Heading 2", value: 2 },
+  { label: "Heading 3", value: 3 },
+  { label: "Heading 4", value: 4 },
+];
+
+export const fontFamilies = [
+  { label: "Arial", value: "Arial", className: "font-[Arial]" },
+  {
+    label: "Times New Roman",
+    value: "Times New Roman",
+    className: "font-[Times New Roman]",
+  },
+  { label: "Verdana", value: "Verdana", className: "font-[Verdana]" },
+  {
+    label: "Courier New",
+    value: "Courier New",
+    className: "font-[Courier New]",
+  },
+  { label: "Georgia", value: "Georgia", className: "font-[Georgia]" },
+  { label: "Geist", value: "Geist", className: "font-[Geist]" },
+  { label: "Impact", value: "Impact", className: "font-[Impact]" },
+  { label: "Roboto", value: "Roboto", className: "font-[Roboto]" },
+  { label: "Open Sans", value: "Open Sans", className: "font-[Open Sans]" },
+  { label: "Montserrat", value: "Montserrat", className: "font-[Montserrat]" },
+  { label: "Lato", value: "Lato", className: "font-[Lato]" },
+];
+
+export const fontSizes = [
+  { label: "12", value: "12px" },
+  { label: "14", value: "14px" },
+  { label: "16", value: "16px" },
+  { label: "18", value: "18px" },
+  { label: "20", value: "20px" },
+  { label: "22", value: "22px" },
+  { label: "24", value: "24px" },
+  { label: "26", value: "26px" },
+  { label: "28", value: "28px" },
+  { label: "30", value: "30px" },
+];

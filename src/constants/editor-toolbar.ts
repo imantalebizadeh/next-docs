a1c25b81b34d23fb8dbc1c802fe39835bfdaa144
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-14 20:37:02 +0330

    feat: add line heights to editor toolbar constants

--- src/constants/editor-toolbar.ts
+++ src/constants/editor-toolbar.ts
@@ -40,3 +40,12 @@ export const fontSizes = [
   { label: "28", value: "28px" },
   { label: "30", value: "30px" },
 ];
+
+export const lineHeights = [
+  { label: "1", value: "1" },
+  { label: "1.5", value: "1.5" },
+  { label: "2", value: "2" },
+  { label: "3", value: "3" },
+  { label: "4", value: "4" },
+  { label: "5", value: "5" },
+];

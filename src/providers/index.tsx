5110e1115ffdffae6800fb44e73661ac4fbceb7f
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-16 22:20:29 +0330

    feat: add Toaster component for notifications in Providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,5 +1,7 @@
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import { EditorStoreProvider } from "./editor-provider";
 import { ThemeProvider } from "./theme-provider";
 
@@ -8,6 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <EditorStoreProvider>
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <NuqsAdapter>{children}</NuqsAdapter>
+        <Toaster />
       </ThemeProvider>
     </EditorStoreProvider>
   );

5abec0f02c22c5863ee30b2cdca3f53a607c2e97
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-17 10:24:33 +0330

    feat: integrate Header and TemplatesSection components into HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Header } from "@/components/layout/header";
+import { TemplatesSection } from "@/components/layout/templates-section";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Header />
+      <TemplatesSection />
+    </div>
+  );
 }

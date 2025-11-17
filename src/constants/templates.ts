603d288013de14c0fec1c6e9ac6a41eec78cc573
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-17 10:24:16 +0330

    feat: add templates for various document types with associated SVG images

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: 1,
+    title: "Blank Document",
+    image: "/svg/blank-document.svg",
+  },
+  {
+    id: 2,
+    title: "Business Letter",
+    image: "/svg/business-letter.svg",
+  },
+  {
+    id: 3,
+    title: "Cover Letter",
+    image: "/svg/cover-letter.svg",
+  },
+  {
+    id: 4,
+    title: "Letter",
+    image: "/svg/letter.svg",
+  },
+  {
+    id: 5,
+    title: "project-proposal",
+    image: "/svg/project-proposal.svg",
+  },
+  {
+    id: 6,
+    title: "Resume",
+    image: "/svg/resume.svg",
+  },
+  {
+    id: 7,
+    title: "Software Proposal",
+    image: "/svg/software-proposal.svg",
+  },
+];

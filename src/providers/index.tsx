f2236e8a15d092513c64da62ce5e2fb401c3e49c
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-19 11:04:59 +0330

    feat: add ConvexProvider to integrate Convex with Clerk in the Providers component

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,17 +1,28 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadcn } from "@clerk/themes";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 import { Toaster } from "@/components/ui/sonner";
 
+import { ConvexProvider } from "./convex-provider";
 import { EditorStoreProvider } from "./editor-provider";
 import { ThemeProvider } from "./theme-provider";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: ReactNode }) {
   return (
-    <EditorStoreProvider>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        <NuqsAdapter>{children}</NuqsAdapter>
-        <Toaster />
-      </ThemeProvider>
-    </EditorStoreProvider>
+    <ClerkProvider appearance={{ theme: shadcn }}>
+      <ConvexProvider>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <EditorStoreProvider>
+            <NuqsAdapter>{children}</NuqsAdapter>
+          </EditorStoreProvider>
+          <Toaster />
+        </ThemeProvider>
+      </ConvexProvider>
+    </ClerkProvider>
   );
 }

e7135b3a6d595471088b1baf24cdd4966ea3b322
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-19 19:14:14 +0330

    feat: replace user icon button with UserButton component from Clerk

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,24 +1,25 @@
 import Link from "next/link";
 
-import { IconUser } from "@tabler/icons-react";
+import { UserButton } from "@clerk/nextjs";
 
 import { Logo } from "../logo";
-import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
 import { SearchInput } from "./search-input";
 
 export function Header() {
   return (
     <header className="flex items-center p-4">
+      {/* Logo */}
       <Link href="/" className="flex items-center gap-2">
         <Logo className="size-10" />
         <p className="font-medium text-xl">Docs</p>
       </Link>
+
+      {/* Search Input */}
       <SearchInput />
-      <div className="flex items-center gap-2">
-        <Button variant="outline" size="icon">
-          <IconUser className="size-4" />
-        </Button>
-      </div>
+
+      {/* User Button */}
+      <UserButton fallback={<Skeleton className="size-8 rounded-full" />} />
     </header>
   );
 }

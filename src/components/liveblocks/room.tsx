37f672f51de014919c09803ac686a3d38d878163
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-27 19:18:04 +0330

    feat: enhance Room component to resolve user data and mention suggestions using usersPromise

--- src/components/liveblocks/room.tsx
+++ src/components/liveblocks/room.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { ReactNode } from "react";
+import { use, type ReactNode } from "react";
 
 import {
   ClientSideSuspense,
@@ -13,12 +13,38 @@ import { DocumentSkeleton } from "@/components/documents/document-skeleton";
 export function Room({
   children,
   documentId,
+  usersPromise,
 }: {
   children: ReactNode;
   documentId: string;
+  usersPromise: Promise<
+    { id: string; email: string; name: string; avatar: string }[]
+  >;
 }) {
+  const users = use(usersPromise);
+
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks">
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks"
+      // biome-ignore lint/nursery/useConsistentArrowReturn: return statement is needed for better readability
+      resolveUsers={({ userIds }) => {
+        return (
+          userIds.map((userId) => users.find((user) => user.id === userId)) ??
+          undefined
+        );
+      }}
+      resolveMentionSuggestions={({ text }) => {
+        if (text) {
+          return users
+            .filter((user) =>
+              user.name.toLowerCase().includes(text.toLowerCase())
+            )
+            .map((user) => user.id);
+        }
+
+        return users.map((user) => user.id);
+      }}
+    >
       <RoomProvider id={documentId}>
         <ClientSideSuspense fallback={<DocumentSkeleton />}>
           {children}

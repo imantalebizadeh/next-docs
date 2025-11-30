055d4d115d32175dcfab692caf32bbcd0c847f38
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-11-30 23:21:01 +0330

    feat: configure Next.js compiler to remove console logs in production

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import "./src/env";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;

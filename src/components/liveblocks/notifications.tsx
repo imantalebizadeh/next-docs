43f017bca2bff719da530392f69dc0268ee26b7f
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-12-10 12:27:42 +0330

    feat: enhance notifications component with mobile support using Drawer and responsive design

--- src/components/liveblocks/notifications.tsx
+++ src/components/liveblocks/notifications.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import type { ComponentProps } from "react";
+
 import {
   ClientSideSuspense,
   useInboxNotifications,
@@ -8,13 +10,21 @@ import {
 import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui";
 import { IconBell } from "@tabler/icons-react";
 
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+
+import { useIsMobile } from "@/hooks/use-is-mobile";
+
+import { cn } from "@/lib/utils";
+
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
+import { DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { ScrollArea } from "../ui/scroll-area";
 import { Skeleton } from "../ui/skeleton";
 
 function Notifications() {
+  const isMobile = useIsMobile();
   const { inboxNotifications } = useInboxNotifications();
 
   return (
@@ -29,7 +39,12 @@ function Notifications() {
           ))}
         </InboxNotificationList>
       ) : (
-        <div className="w-lg rounded-md py-4 text-center text-muted-foreground text-sm">
+        <div
+          className={cn(
+            "w-lg rounded-md py-4 text-center text-muted-foreground text-sm",
+            !!isMobile && "h-16 w-auto"
+          )}
+        >
           No notifications
         </div>
       )}
@@ -38,33 +53,69 @@ function Notifications() {
 }
 
 function NotificationsButton() {
+  const isMobile = useIsMobile();
   const { count } = useUnreadInboxNotificationsCount();
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon-sm"
-          className="relative rounded-full"
-        >
-          <span className="sr-only">Notifications</span>
-          <IconBell />
-          {count > 0 && (
-            <Badge className="-top-1 -right-1 absolute h-5 min-w-5 rounded-full px-1 font-mono tabular-nums">
-              {count}
-            </Badge>
-          )}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align="end" className="w-full max-w-lg p-px">
-        <ScrollArea className="h-72">
-          <ClientSideSuspense fallback={<NotificationsSkeleton />}>
-            <Notifications />
-          </ClientSideSuspense>
-        </ScrollArea>
-      </PopoverContent>
-    </Popover>
+    <>
+      {isMobile ? (
+        <Drawer>
+          <DrawerTrigger asChild>
+            <NotificationTrigger />
+          </DrawerTrigger>
+          <DrawerContent>
+            <DialogHeader className="sr-only">
+              <DialogTitle>Notifications</DialogTitle>
+              <DialogDescription>
+                You have {count} unread notifications
+              </DialogDescription>
+            </DialogHeader>
+            <ScrollArea className={cn("h-96", count === 0 && "h-auto")}>
+              <ClientSideSuspense fallback={<NotificationsSkeleton />}>
+                <Notifications />
+              </ClientSideSuspense>
+            </ScrollArea>
+          </DrawerContent>
+        </Drawer>
+      ) : (
+        <Popover>
+          <PopoverTrigger asChild>
+            <NotificationTrigger />
+          </PopoverTrigger>
+          <PopoverContent align="end" className="w-full max-w-lg p-px">
+            <ScrollArea className={cn("h-72", count === 0 && "h-auto")}>
+              <ClientSideSuspense fallback={<NotificationsSkeleton />}>
+                <Notifications />
+              </ClientSideSuspense>
+            </ScrollArea>
+          </PopoverContent>
+        </Popover>
+      )}
+    </>
+  );
+}
+
+function NotificationTrigger({
+  className,
+  ...props
+}: ComponentProps<typeof Button>) {
+  const { count } = useUnreadInboxNotificationsCount();
+
+  return (
+    <Button
+      variant="outline"
+      size="icon-sm"
+      className={cn("relative rounded-full", className)}
+      {...props}
+    >
+      <span className="sr-only">Notifications</span>
+      <IconBell />
+      {count > 0 && (
+        <Badge className="-top-1 -right-1 absolute h-5 min-w-5 rounded-full px-1 font-mono tabular-nums">
+          {count}
+        </Badge>
+      )}
+    </Button>
   );
 }
 
